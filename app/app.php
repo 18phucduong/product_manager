53d6b6b6470c68c06b7cecc13ed80611a421b230
18phucduong <phuc.duong@alobridge.com>
2022-03-02 16:51:40 +0700

    them router, chua xu ly controller cho route

--- app/app.php
+++ app/app.php
@@ -1,11 +1,30 @@
 <?php
 
+require "core/Router.php";
+use app\Router;
+
 class App {
 
+	private $router;
+
 	public function __construct() {
+		$this->router = new Router;
 
-		echo "app is running";
-
+		$this->router->get('/', function() {
+			echo "return admin view";
+		});
+		$this->router->get('/login', function() {
+			echo "return login view";
+		});
+		
+		$this->router->get('/product/new', function() {
+			echo "return new product view";
+		});
+		$this->router->post('/product/new', function() {
+			echo "process new product ";
+		});
+		
+		$this->router->run();
 	}
 
 }
---
+++ app/core/Router.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace app;
+
+class Router {
+	
+	public $routers = array();
+
+	public function __construct() {
+
+	}
+
+	private function get_request_url() {
+
+		$url = $_SERVER['REQUEST_URI'];
+		$url = str_replace('product_manager/public/', '', $url);
+		return $url;
+	}
+
+	private function get_request_method(){
+		$method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
+		return $method;
+	}
+
+	public function add_router($method, $url, $action) {
+		$this->routers[] = [$method, $url, $action];
+	}
+
+	public function get($url, $action) {
+		$this->add_router('GET', $url, $action);
+	}
+	public function post($url, $action) {
+		$this->add_router('POST', $url, $action);
+	}
+	public function delete($url, $action) {
+		$this->add_router('DELETE', $url, $action);
+	}
+
+	public function map_routers() {
+		$request_url = $this->get_request_url();
+		$request_method = $this->get_request_method();
+
+		$routes = $this->routers;
+
+		echo "<pre>";
+		print_r($routes);
+		echo "</pre>";
+
+	}
+
+	public function run() {
+		$this->map_routers();
+	}
+}
