e35287f53dceb6dcc338ef6417369287ad1c1045
18phucduong <phuc.duong@alobridge.com>
2022-03-03 10:15:03 +0700

    create html for login.php

--- app/views/login.php
+++ app/views/login.php
@@ -1,1 +1,26 @@
-s
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="login-form mx-auto">
+                        <form action="" method="POST">
+                            <input type="text" name="user_name" placeholder="Enter user_name">
+                            <input type="password" name="password" placeholder="Enter password">
+                            <input type="submit" class="button" value="LOGIN">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
