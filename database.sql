079728b25d776fc7bf3e8f8c4ea0e4a50f146600
18phucduong <phuc.duong@alobridge.com>
2022-03-03 08:59:22 +0700

    PRO #6441 change tables name and add primary to product_tag table

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `full_name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE `user` (
 -- Table structure for table `product`
 --
 
-CREATE TABLE `product` (
+CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `price` int NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE `product` (
 -- Table structure for table `tag`
 --
 
-CREATE TABLE `tag` (
+CREATE TABLE `tags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `slug` varchar(50) NOT NULL,
@@ -49,7 +49,8 @@ CREATE TABLE `tag` (
 CREATE TABLE `product_tag` (
   `product_id` int(20) NOT NULL,
   `tag_id` int(20) NOT NULL,
+  PRIMARY KEY( `product_id`, `tag_id`),
   FOREIGN KEY (`product_id`) REFERENCES `product`(`id`),
   FOREIGN KEY (`tag_id`) REFERENCES `tag`(`id`),
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- -----------------------------------------------
+-- ----------------------------------------------- 
