1eaf558a0512951ee92f045797af28e45ba763e2
18phucduong <phuc.duong@alobridge.com>
2022-03-03 11:17:03 +0700

    add html code to login.php  and css to style.css

---
+++ app/views/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" integrity="sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="wrapper login-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="login-form mx-auto">
+                        <form action="" method="POST">
+                            <input type="text" name="user_name" placeholder="Enter user_name">
+                            <input type="password" name="password" placeholder="Enter password">
+                            <input type="submit" class="button" value="LOGIN">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
