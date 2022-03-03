19ea66159e155d3a986058974635c6e9766c682e
18phucduong <100657200+18phucduong@users.noreply.github.com>
2022-03-03 16:01:40 +0700

    Task/#6443 create login page (#2)

---
+++ app/views/login.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Login</title>
+</head>
+<body>
+    <div class="wrapper login-page">
+        <div class="container">
+            <div class="row">
+                <div class="col col-12">
+                    <div class="login-form">
+                        <div class="login-form__inner">
+                            <div class="row row-collapse ">
+                                <div class="col col-6 sm-12 hide-on-small">
+                                    <div class="left-form dark text-center">
+                                        <div class="left-form__img">
+                                            <img src="https://picsum.photos/id/237/500/270" alt="">
+                                        </div>
+                                        <div class="left-form__text mx-auto">
+                                            <h2 class="mb-half">Change your quantity life</h2>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam laeriores</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col col-6 sm-12">
+                                    <div class="right-form  mx-auto ">
+                                        <div class="right-form__head text-center mb-2">
+                                            <h1 class="mb-1">Hello Again!</h1>
+                                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration i</p>
+                                        </div>
+                                        <div class="right-form__main mb-2">
+                                            <form action="" method="post">
+                                                <div class="input-group round float-label">
+                                                    <input type="text" name="user_name" id="user_name" required >
+                                                    <label for="user_name">Username</label>
+                                                    <i class="fa-solid fa-at"></i>
+                                                </div>
+                                                <div class="input-group round float-label">
+                                                    <input type="password" name="password" id="password" required >
+                                                    <label for="password">Password</label>
+                                                    <i class="fa-solid fa-key"></i>
+                                                </div>
+                                                <div class="row row-collapse mb-2">
+                                                    <div class="col col-6">
+                                                        <div class="input-group">
+                                                            <input type="checkbox" name="remember_pass" id="remember_pass">
+                                                            <label for="remember_pass" class="is-small color-gray">Remember password</label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col col-6 text-right">
+                                                        <a href="#" class="link is-small">Recovary password</a>
+                                                    </div>
+                                                </div>
+                                                <div class="input-group">
+                                                    <input type="submit" class="button expand round" name="login" value="LOGIN">
+                                                </div>
+                                                <div class="input-group">
+                                                    <button  class="button expand round outline gray">
+                                                        <img src="assets/images/google-icon.png" alt="">
+                                                       Sign in with google
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="right-form__bottom text-center">
+                                            <p>Do u have an account yet? <a href="#" class="link">Sign Up</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
