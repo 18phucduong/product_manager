d6f7db8614bc6ca27ee6ad03da847d3af257fe8e
18phucduong <phuc.duong@alobridge.com>
2022-03-18 14:34:38 +0700

    #PRO #6478 Show product and pagination ajax

---
+++ app/controllers/api/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\controllers\api;
+
+use app\models\Product;
+use app\core\Database;
+
+class ProductController {
+
+    public function index() {
+        $page = intval($_POST['page']);
+        $pagination = Database::table('products')->pagination(10, $page);
+        
+        view('product/products-table',['products' => $pagination]);
+    }
+    public function delete($id)  {
+        
+    }
+}
